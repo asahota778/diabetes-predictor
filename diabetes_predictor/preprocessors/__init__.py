68ce977a9c31c98ba8c986937272e4449b68416b
asahota778 <108929229+asahota778@users.noreply.github.com>
2024-10-21 17:50:46 +0200

    Update __init__.py

--- diabetes_predictor/preprocessors/__init__.py
+++ diabetes_predictor/preprocessors/__init__.py
@@ -1,1 +1,6 @@
-from diabetes_predictor.preprocessors.drop_na_preprocessor import DropNaPreprocessor
+# diabetes_predictor/preprocessors/__init__.py
+from .drop_na_preprocessor import DropNaPreprocessor
+from .fill_na_preprocessor import FillNaPreprocessor
+
+__all__ = ['DropNaPreprocessor', 'FillNaPreprocessor']
+
